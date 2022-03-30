0928292fb1ab5c526a3e121284a7ff38e224c994
takeshi_honda <honda0924@hotmail.co.jp>
2022-03-31 00:25:54 +0900

    Initial commit

---
+++ resources/js/app.ts
@@ -0,0 +1,11 @@
+import "vuetify/styles";
+import { createApp } from "vue";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import App from "./components/App.vue";
+
+const app = createApp(App);
+const vuetify = createVuetify({ components, directives });
+app.use(vuetify);
+app.mount("#app");
